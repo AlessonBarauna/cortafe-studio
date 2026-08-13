a273bb55b98ed56baaf7cd845a78d2de9798082b
Alesson Barauna <alesson.barauna1@gmail.com>
2026-08-12 23:15:22 -0300

    Acelera processamento dos vídeos

---
+++ tests/CortaFeStudio.Tests/YouTubeAcquisitionTests.cs
@@ -0,0 +1,23 @@
+using CortaFeStudio.Api.Services;
+
+namespace CortaFeStudio.Tests;
+
+public sealed class YouTubeAcquisitionTests
+{
+    [Fact]
+    public void Download_PrefereH264EFragmentosConcorrentes()
+    {
+        var args = YouTubeAcquisition.DownloadArguments(["--force-ipv4"], "ffmpeg", "source.%(ext)s", "https://youtu.be/teste");
+        Assert.Contains("--concurrent-fragments", args);
+        Assert.Contains("4", args);
+        Assert.Contains("vcodec^=avc1", string.Join(' ', args));
+    }
+
+    [Fact]
+    public void Metadados_NaoBaixamVideo()
+    {
+        var args = YouTubeAcquisition.MetadataArguments([], "https://youtu.be/teste");
+        Assert.Contains("--skip-download", args);
+        Assert.Contains("duration", args);
+    }
+}
